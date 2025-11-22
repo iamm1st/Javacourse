bae153725fc8d35e9af676fc415252afa8de4055
Polina <polina.18.06@mail.ru>
2025-11-22 19:01:30 +0300

    В переменных q и w хранятся два натуральных числа. Создайте программу, выводящую на экран результат деления q на w с остатком. Пример вывода программы (для случая, когда в q хранится 21, а в w хранится 8): 21 / 8 = 2 и 5 в остатке

---
+++ src/lesson1/exerc5.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class exerc5 {
+    public static void main(String[] args) {
+
+        int q = 21;
+        int w = 8;
+        int a = q % w;
+        int b = q / w;
+
+        System.out.println(q + "/" + w + " = " + b + " и " + a + " в остатке ");
+
+    }
+}
