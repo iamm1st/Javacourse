b8e970ac56bec277b6ec88af99530d77d94ae92f
Polina <polina.18.06@mail.ru>
2025-11-22 19:18:38 +0300

    Необходимо, чтоб программа выводила на экран вот такую последовательность: 7 14 21 28 35 42 49 56 63 70 77 84 91 98. В решении используйте цикл while.

---
+++ src/lesson2/exerc4.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class exerc4 {
+    public static void main (String[] args) {
+
+        int a = 7;
+        System.out.printf(a + " ");
+        while (a < 98) {
+            a = a + 7;
+            System.out.printf(a + " ");
+        }
+    }
+}
