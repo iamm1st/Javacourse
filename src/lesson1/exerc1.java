18ea73f1c4eb11854c2bef41ed23ae7cafcf820b
Polina <polina.18.06@mail.ru>
2025-12-07 22:23:06 +0300

    Corrections (ex 1)

--- src/lesson1/exerc1.java
+++ src/lesson1/exerc1.java
@@ -4,11 +4,10 @@ public class exerc1 {
 
     public static void main(String[] args) {
 
-        int a;
         int b = 5;
         int c = 15;
 
-        a = 4 * (b + c - 1) / 2;
+       int a = 4 * (b + c - 1) / 2;
 
         System.out.println(a);
 
