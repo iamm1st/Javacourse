363e299aa0ffee3d5568886a7c8c8af99a85e266
Polina <polina.18.06@mail.ru>
2025-12-07 22:41:02 +0300

    Corrections (ex 4)

--- src/lesson2/exerc4.java
+++ src/lesson2/exerc4.java
@@ -3,8 +3,7 @@ package lesson2;
 public class exerc4 {
     public static void main (String[] args) {
 
-        int a = 7;
-        System.out.printf(a + " ");
+        int a = 0;
         while (a < 98) {
             a = a + 7;
             System.out.printf(a + " ");
