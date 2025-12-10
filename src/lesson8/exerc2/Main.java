9efe07f7ba544769b3265efd4ebae032d6462387
Polina <polina.18.06@mail.ru>
2025-12-10 03:16:36 +0300

    Создать класс Apple и добавить в него поле color с модификатором доступа private и инициализировать его. В методе main другого класса создать объект Apple, и не используя сеттеры изменить значение поля color.

---
+++ src/lesson8/exerc2/Main.java
@@ -0,0 +1,12 @@
+package lesson8.exerc2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Apple apple = new Apple();
+
+        apple.printCol();
+        apple.changeCol("Green");
+        apple.printCol();
+    }
+}
