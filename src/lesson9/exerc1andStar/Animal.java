fbef0191fccf0c361d008e4a1d12837a72e8782a
Polina <polina.18.06@mail.ru>
2025-12-14 21:18:58 +0300

    Задание 1: Создать логически верную иерархию следующих классов: Animal, Dog, Tiger, Rabbit. Переопределить методы voice(), eat(String food) чтобы они выводили верную информацию. Метод eat может принимать “Meat”, “Grass” и другие строки. Например, если мы создадим объект класса Rabbit, вызовем метод eat() и передадим туда “Grass” он должен написать в консоли что любит есть травку. Если передадим “Meat” или другую строку то он будет недоволен. Задача *: Написать такой конструктор, который запретит создание объекта класса Dog в других классах; Найти способ вызывать методы voice(), eat() класса Dog, который позволит обойти ограничение, наложенное конструктором выше. Объяснить, как работают оба способа.

---
+++ src/lesson9/exerc1andStar/Animal.java
@@ -0,0 +1,12 @@
+package lesson9.exerc1andStar;
+
+public class Animal {
+
+    public void voice() {
+        System.out.println("Animal makes a sound");
+    }
+
+    public void eat(String food) {
+        System.out.println("Animal eats " + food);
+    }
+}
