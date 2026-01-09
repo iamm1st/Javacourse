e948531ffd07e59f193af334b2895cf93926b6fd
Polina <polina.18.06@mail.ru>
2026-01-09 20:46:53 +0300

    Вывести в консоль из строки которую пользователь вводит с клавиатуры все аббревиатуры. Аббревиатурой будем считать слово от 2 до 6 символов, состоящее только из прописных букв, без чисел

---
+++ src/exerc1/Regex1.java
@@ -0,0 +1,24 @@
+package exerc1;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите строку:");
+        String text = scanner.nextLine();
+
+        Pattern abPattern = Pattern.compile("\\b[A-Z]{2,6}\\b");
+        Matcher abMatcher = abPattern.matcher(text);
+
+        System.out.println("Найденные аббревиатуры:");
+        while (abMatcher.find()) {
+            System.out.println(abMatcher.group());
+        }
+    }
+}
