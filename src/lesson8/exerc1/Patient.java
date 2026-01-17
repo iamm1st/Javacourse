ae9668fe3b9ba293f329269ade21e97aef586b92
Polina <polina.18.06@mail.ru>
2026-01-17 17:38:04 +0300

    getters/setters added

--- src/lesson8/exerc1/Patient.java
+++ src/lesson8/exerc1/Patient.java
@@ -1,6 +1,23 @@
 package lesson8.exerc1;
 
 class Patient {
-    int treatmentPlan;
-    Doctor doctor;
+
+    private int treatmentPlan;
+    private Doctor doctor;
+
+    public int getTreatmentPlan() {
+        return treatmentPlan;
+    }
+
+    public void setTreatmentPlan(int treatmentPlan) {
+        this.treatmentPlan = treatmentPlan;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
--- src/lesson8/exerc1/Therapist.java
+++ src/lesson8/exerc1/Therapist.java
@@ -1,20 +1,24 @@
 package lesson8.exerc1;
 
 class Therapist extends Doctor {
+
     @Override
     void treat() {
         System.out.println("Терапевт проводит общее лечение.");
     }
 
     void assignDoctor(Patient patient) {
-        if (patient.treatmentPlan == 1) {
-            patient.doctor = new Surgeon();
-        } else if (patient.treatmentPlan == 2) {
-            patient.doctor = new Dentist();
+
+        if (patient.getTreatmentPlan() == 1) {
+            patient.setDoctor(new Surgeon());
+
+        } else if (patient.getTreatmentPlan() == 2) {
+            patient.setDoctor(new Dentist());
+
         } else {
-            patient.doctor = new Therapist();
+            patient.setDoctor(new Therapist());
         }
 
-        patient.doctor.treat();
+        patient.getDoctor().treat();
     }
 }
